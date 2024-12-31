065e146e5367453d525c8411ea5f1790eb89df1a
Pranav Malhotra <pranav.malhotra1903@gmail.com>
2024-12-31 22:54:44 +0530

    start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import ButtonGradient from './assets/assets/svg/ButtonGradient'
+
+const App =() => {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <ButtonGradient />
+    </>
+    
+    
+  )
+}
+
+export default App
